01d68f881f74a3318dc95edd6def33243ca0c9c9
Otavio Guerra <otaviomguerra@hotmail.com>
2024-02-25 13:32:40 -0300

    solved leap year exercise

--- leap/leap.go
+++ leap/leap.go
@@ -5,5 +5,9 @@ package leap
 
 // IsLeapYear checks if a given year is a leap year.
 func IsLeapYear(year int) bool {
-
+	if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
+		return true
+	} else {
+		return false
+	}
 }
